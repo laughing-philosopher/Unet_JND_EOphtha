fcee36a30e5d3e44554c08a451aaf65ed1382960
AyushhS <ayush77.2000@gmail.com>
2024-05-03 21:17:49 +0530

    increased threshold

--- processing.py
+++ processing.py
@@ -92,6 +92,6 @@ def processing(img):
         cnt2 = cnt2 + 1
 
     psm_th2 = final_res / np.max(final_res)
-    psm_th2 = (psm_th2 > 0.05).astype(float)
+    psm_th2 = (psm_th2 > 0.1).astype(float)
 
     return psm_th2
