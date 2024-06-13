d3e0d9fa470bd9d7f12a77942063312751c7d2b9
AyushhS <ayush77.2000@gmail.com>
2024-06-13 11:32:22 +0530

    added batched processing

--- processing.py
+++ processing.py
@@ -1,27 +1,7 @@
-import os
 import cv2
-import random
 import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
-from glob import glob
-from tqdm import tqdm
-# from patchify import patchify
-from natsort import natsorted
-# from patchify import unpatchify
-from tensorflow.keras.models import load_model
-from tensorflow.keras.optimizers import Adam
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-from tensorflow.keras import backend as K
-from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input, Dropout, Lambda, Add, Multiply
 
 from model import build_UNet
-from functions import ce_loss, acc_met, sensitivity, specificity
 
 # Patch dimension details
 Img_Width = 48
@@ -31,9 +11,7 @@ Img_Channels = 1
 model = build_UNet((Img_Width, Img_Height, Img_Channels),(Img_Width, Img_Height, Img_Channels))
 model.load_weights('UNet_JND_EOphtha.h5')
 
-def processing(img, threshold):
-    print(threshold)
-    print(img.shape)
+def processing(img, threshold, batch_size):
     img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # The initial processing of the image
@@ -49,6 +27,9 @@ def processing(img, threshold):
     gt = gt / 255.0
     gt = (gt > 0.5)
 
+
+    iarr = []
+    jarr = []
     tot = 0
     step = 8
     for i in range(0,m,step):
@@ -56,41 +37,84 @@ def processing(img, threshold):
             if((i+48)>(m-1) or (j+48)>(n-1)):
                 pass
             else:
-                tot = tot + 1
-     # increase this threshold if more false positives are coming
-    cnt1 = 0
-    cnt2 = 0
-    iarr = []
-    jarr = []
-    patches_img = np.zeros((tot,48,48,1))
-    patches_gt = np.zeros((tot,48,48,1))
-    final_res = np.zeros((m,n))
-
-    for i in range(0,m,step):
-        for j in range(0,n,step):
-            if((i+48)>(m-1) or (j+48)>(n-1)):
-                pass
-            else:
-                # getting image patches
-                patch_img = img_c[i:i+48,j:j+48]
-                patch_img = np.expand_dims(patch_img, axis=-1)
-                patch_img = np.expand_dims(patch_img, axis=0)
-                patches_img[cnt1] = patch_img
-                # getting gt patches
-                patch_gt = gt[i:i+48,j:j+48]
-                patch_gt = np.expand_dims(patch_gt, axis=-1)
-                patch_gt = np.expand_dims(patch_gt, axis=0)
-                patches_gt[cnt1] = patch_gt
-                cnt1 = cnt1 + 1
                 iarr.append(i)
                 jarr.append(j)
+                tot = tot + 1
+    #  # increase this threshold if more false positives are coming
+    
+    
+    # batch_size = 10
 
-    inter_res,_ = model.predict([patches_img,patches_gt],verbose=False)
-    inter_res = (inter_res > threshold)
-    for k in range(cnt1):
-        final_res[iarr[k]:(iarr[k]+48),jarr[k]:(jarr[k]+48)] = final_res[iarr[k]:iarr[k]+48,jarr[k]:jarr[k]+48] + np.squeeze(inter_res[cnt2])
-        cnt2 = cnt2 + 1
 
+
+    current_batch_size = tot // batch_size
+    cnt1 = 0
+    cnt2 = 0
+    final_res = np.zeros((m,n))
+    for batch in range(batch_size):
+        
+        patches_img = np.zeros((current_batch_size,48,48,1))
+        patches_gt = np.zeros((current_batch_size,48,48,1))
+        
+
+        for k in range(current_batch_size):
+            # getting image patches
+            itr = k + batch * current_batch_size
+            if ((iarr[itr] + 48) > (m - 1) or (jarr[itr] + 48) > (n - 1)):
+                pass
+            else:
+                patch_img = img_c[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)]
+                patch_img = np.expand_dims(patch_img, axis=-1)
+                patch_img = np.expand_dims(patch_img, axis=0)
+                patches_img[k] = patch_img
+                # getting gt patches
+                patch_gt = gt[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)]
+                patch_gt = np.expand_dims(patch_gt, axis=-1)
+                patch_gt = np.expand_dims(patch_gt, axis=0)
+                patches_gt[k] = patch_gt
+                    
+        print(patches_img.shape, batch*current_batch_size)
+        inter_res,_ = model.predict([patches_img,patches_gt],verbose=False)
+        # print(inter_res)
+        # inter_res = (inter_res > threshold)
+        for k in range(current_batch_size):
+            itr=k+batch*current_batch_size
+            final_res[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)] = final_res[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)] + np.squeeze(inter_res[k])
+            
+        del patches_img, patches_gt, inter_res
+    
+    if (tot % batch_size != 0):
+        current_batch_size = tot % batch_size
+        patches_img = np.zeros((current_batch_size,48,48,1))
+        patches_gt = np.zeros((current_batch_size,48,48,1))
+        
+
+        for k in range(current_batch_size):
+            # getting image patches
+            itr = k + (tot - current_batch_size)
+            if ((iarr[itr] + 48) > (m - 1) or (jarr[itr] + 48) > (n - 1)):
+                pass
+            else:
+                patch_img = img_c[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)]
+                patch_img = np.expand_dims(patch_img, axis=-1)
+                patch_img = np.expand_dims(patch_img, axis=0)
+                patches_img[k] = patch_img
+                # getting gt patches
+                patch_gt = gt[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)]
+                patch_gt = np.expand_dims(patch_gt, axis=-1)
+                patch_gt = np.expand_dims(patch_gt, axis=0)
+                patches_gt[k] = patch_gt
+                    
+        print(patches_img.shape, batch*current_batch_size)
+        inter_res,_ = model.predict([patches_img,patches_gt],verbose=False)
+        # print(inter_res)
+        # inter_res = (inter_res > threshold)
+        for k in range(current_batch_size):
+            itr=k+(tot - current_batch_size)
+            final_res[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)] = final_res[iarr[itr]:(iarr[itr]+48),jarr[itr]:(jarr[itr]+48)] + np.squeeze(inter_res[k])
+            
+        del patches_img, patches_gt, inter_res
+    
     psm_th2 = final_res / np.max(final_res)
     psm_th2 = (psm_th2 > 0.1).astype(float)
 
--- streamlit.py
+++ streamlit.py
@@ -12,6 +12,7 @@ def main():
     st.title("Unet_JND_EOphtha")
 
     threshold = st.number_input('Enter the threshold value between 0.5 and 0.99 (recommended value = 0.9)', min_value=0.5, max_value=0.99)
+    batch_size = st.number_input('Enter Batch Size (larger batch size will be slower but more stable, recommended value = 4)', min_value=2)
     uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
     if uploaded_image is not None:
@@ -20,7 +21,7 @@ def main():
         image_cv2 = np.array(image.convert('RGB'))
         st.image(image_cv2)
         
-        output_image = processing(image_cv2, threshold)
+        output_image = processing(image_cv2, threshold, batch_size)
         # blue = np.zeros(image_cv2.shape)
         # for i in range(0, len(output_image)):
         #     for j in range(0, len(output_image[0])):
